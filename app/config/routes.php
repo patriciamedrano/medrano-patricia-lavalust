510d7c8242bfb6a3046b0caf5572546693be56ea
Patricia Medrano <patriciamedrano502@gmail.com>
2026-08-18 19:26:08 +0800

    update Dockerfile

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,6 +2,8 @@
 
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
+$router->get('/', 'StudentController::index');
+
 $router->get('/student', 'StudentController::index');
 
 $router->get('/student/profile', 'StudentController::profile')
