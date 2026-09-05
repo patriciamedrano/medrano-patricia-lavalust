09e32d5080fb27e524248046146dc07b93ac4104
Patricia Medrano <patriciamedrano502@gmail.com>
2026-09-05 16:27:03 +0800

    Add users MVC and route

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,9 +2,11 @@
 
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
-$router->get('/', 'StudentController::index');
+$router->get('/git', 'StudentController::index');
 
 $router->get('/student', 'StudentController::index');
 
 $router->get('/student/profile', 'StudentController::profile')
        ->middleware('student');
+
+$router->get('/users', 'UsersController::index');
